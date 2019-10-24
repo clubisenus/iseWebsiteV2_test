1dd748875aa078b15f019780dc069632ef8f3f85
daniel <cookiedan42@gmail.com>
2019-10-24 22:08:16 +0800

    Updates

--- precache-manifest.76ea26b7e1465e47fc340e375b1ada97.js
+++ precache-manifest.76ea26b7e1465e47fc340e375b1ada97.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "77e9a9cee642a3007b7125bcfef0ab74",
+    "revision": "8cf42631059c0353f92f091ba98f22bb",
     "url": "https://clubisenus.github.io/iseWebsiteV2_test/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://clubisenus.github.io/iseWebsiteV2_test/static/js/2.f45effee.chunk.js"
   },
   {
-    "revision": "129ed34a931635fc0328",
-    "url": "https://clubisenus.github.io/iseWebsiteV2_test/static/js/main.fd969bc5.chunk.js"
+    "revision": "9ebc05dcef821e3e8ba1",
+    "url": "https://clubisenus.github.io/iseWebsiteV2_test/static/js/main.df8bd835.chunk.js"
   },
   {
     "revision": "293f2037bad085bd2054",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://clubisenus.github.io/iseWebsiteV2_test/precache-manifest.3cf4124d61b0289d52ab49c5eb86eec3.js"
+  "https://clubisenus.github.io/iseWebsiteV2_test/precache-manifest.76ea26b7e1465e47fc340e375b1ada97.js"
 );
 
 self.addEventListener('message', (event) => {
